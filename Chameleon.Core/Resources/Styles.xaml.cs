ba2070e128e432a2fa010fe28fb7ccf844a9b1ac
Rick van Dijk <rickavdijk@gmail.com>
2019-07-24 16:08:49 +0200

    CHAM-6 styles for buttons added

---
+++ Chameleon.Core/Resources/Styles.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Chameleon.Core.Resources
+{
+    public partial class Styles : ResourceDictionary
+    {
+        public Styles()
+        {
+            InitializeComponent();
+        }
+    }
+}

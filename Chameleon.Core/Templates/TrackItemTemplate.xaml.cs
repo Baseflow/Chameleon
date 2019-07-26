06e3d169b9f7da448481fd2d2b92f9c75415e44d
Rick van Dijk <rickavdijk@gmail.com>
2019-07-26 15:10:14 +0200

    CHAM-27 templateupdated, queuepage layout set

--- Chameleon.Core/Templates/TrackItemTemplate.xaml.cs
+++ Chameleon.Core/Templates/TrackItemTemplate.xaml.cs
@@ -5,9 +5,9 @@ using Xamarin.Forms;
 
 namespace Chameleon.Core.Templates
 {
-    public partial class TrackListTemplate : DataTemplate
+    public partial class TrackItemTemplate : DataTemplate
     {
-        public TrackListTemplate()
+        public TrackItemTemplate()
         {
             InitializeComponent();
         }

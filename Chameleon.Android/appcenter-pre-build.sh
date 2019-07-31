75ea1dd6ea49d927bf628f60a32cd521ea8744cd
Martijn van Dijk <mhvdijk@gmail.com>
2019-07-31 17:17:55 +0200

    Add scripts

---
+++ Chameleon.Android/appcenter-pre-build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# For Xamarin Android, change the version name located in AndroidManifest.xml. 
+# AN IMPORTANT THING: YOU NEED DECLARE PACKAGE_NAME AND VERSION_NAME ENVIRONMENT VARIABLES IN APP CENTER BUILD CONFIGURATION.
+
+if [ ! -n "$PACKAGE_NAME" ]
+then
+    echo "You need to define the PACKAGE_NAME variable in App Center"
+    exit
+fi
+
+if [ ! -n "$VERSION_NAME" ]
+then
+    echo "You need to define the VERSION_NAME variable in App Center"
+    exit
+fi
+
+ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/Chameleon.Android/Properties/AndroidManifest.xml
+VERSION=${VERSION_NAME}.${APPCENTER_BUILD_ID}
+if [ ${APPCENTER_BRANCH} != "master" ]
+then
+    VERSION=${VERSION}.${APPCENTER_BRANCH}
+fi
+
+if [ -e "$ANDROID_MANIFEST_FILE" ]
+then
+    echo "Updating package name to $PACKAGE_NAME in AndroidManifest.xml"
+    sed -i '' 's/package="[a-z.]*"/package="'$PACKAGE_NAME'"/' $ANDROID_MANIFEST_FILE
+
+    echo "Updating version name to $VERSION in AndroidManifest.xml"
+    sed -i '' 's/versionName="[0-9.]*"/versionName="'${VERSION}'"/' $ANDROID_MANIFEST_FILE
+
+    echo "Updating version code to $APPCENTER_BUILD_ID in AndroidManifest.xml"
+    sed -i '' 's/versionCode="[0-9.]*"/versionCode="'${APPCENTER_BUILD_ID}'"/' $ANDROID_MANIFEST_FILE
+
+    # Update the App Icon if an override is specified as environment variable
+    if [ -n "$LAUNCH_ICON" ]
+    then
+        echo "Updating the App Launch icon to ${LAUNCH_ICON}"
+        sed -i '' 's/ic_launcher/'${LAUNCH_ICON}'/' $ANDROID_MANIFEST_FILE
+    fi
+
+    echo "File content:"
+    cat $ANDROID_MANIFEST_FILE
+fi
---
+++ Chameleon.iOS/appcenter-pre-build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# For Xamarin iOS, change the version name located in Info.plist. 
+# AN IMPORTANT THING: YOU NEED DECLARE PACKAGE_NAME AND VERSION_NAME ENVIRONMENT VARIABLES IN APP CENTER BUILD CONFIGURATION.
+
+if [ ! -n "$PACKAGE_NAME" ]
+then
+    echo "You need define the PACKAGE_NAME variable in App Center"
+    exit
+fi
+
+if [ ! -n "$VERSION_NAME" ]
+then
+    echo "You need define the VERSION_NAME variable in App Center"
+    exit
+fi
+
+INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/Chameleon.iOS/Info.plist
+VERSION=${VERSION_NAME}.${APPCENTER_BUILD_ID}
+SHORT_VERSION=${VERSION_NAME}
+
+if [ ${APPCENTER_BRANCH} != "master" ]
+then
+    SHORT_VERSION=${SHORT_VERSION}.${APPCENTER_BRANCH}
+fi
+
+if [ -e "$INFO_PLIST_FILE" ]
+then
+    echo "Updating package name to $PACKAGE_NAME in Info.plist"
+    plutil -replace CFBundleIdentifier -string $PACKAGE_NAME $INFO_PLIST_FILE
+
+    echo "Updating short version string to $SHORT_VERSION in Info.plist"
+    plutil -replace CFBundleShortVersionString -string $SHORT_VERSION $INFO_PLIST_FILE
+
+    echo "Updating version string to ${VERSION}"
+    plutil -replace CFBundleVersion -string $VERSION $INFO_PLIST_FILE
+
+    if [ -n "$LAUNCH_ICON" ]
+    then
+        echo "Updating the App Icon to ${LAUNCH_ICON}"
+        plutil -replace XSAppIconAssets -string ${LAUNCH_ICON} $INFO_PLIST_FILE
+    fi
+
+    echo "File content:"
+    cat $INFO_PLIST_FILE
+fi

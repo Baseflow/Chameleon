1ad4d62cc722c077beb70c6ce087c280b71af46c
Rick van Dijk <rickavdijk@gmail.com>
2019-08-02 17:07:11 +0200

    CHAM-41 android searchbar renderer added

---
+++ Chameleon.Android/CustomRenderers/MySearchBarRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.Content;
+using Android.Widget;
+using Android.Graphics;
+using Chameleon.Android.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Views;
+
+[assembly: ExportRenderer(typeof(SearchBar), typeof(MySearchBarRenderer))]
+namespace Chameleon.Android.CustomRenderers
+{
+    public class MySearchBarRenderer : SearchBarRenderer
+    {
+        public MySearchBarRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+
+            SearchView searchView = (base.Control as SearchView);
+
+            int textViewId = searchView.Context.Resources.GetIdentifier("android:id/search_src_text", null, null);
+            EditText textView = (searchView.FindViewById(textViewId) as EditText);
+            if (textView != null)
+            {
+                int searchMagIcon = searchView.Context.Resources.GetIdentifier("android:id/search_mag_icon", null, null);
+                ImageView magIcon = (ImageView)searchView.FindViewById(searchMagIcon);
+                magIcon.SetColorFilter(global::Android.Graphics.Color.Rgb(255, 255, 255));
+            }
+        }
+    }
+}

b0b8abb2926f4ad9a8b25d4e809530914ad546f0
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-05 17:03:10 +0200

    Add mac project

---
+++ Chameleon.Mac/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace Chameleon.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
+

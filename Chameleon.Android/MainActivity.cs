572019b34ea1fc4b01c2ea3348c6ddbbdc65db76
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-07 14:56:05 +0200

    Add app sharing

--- Chameleon.Android/MainActivity.cs
+++ Chameleon.Android/MainActivity.cs
@@ -11,15 +11,25 @@ using Chameleon.Core;
 using MediaManager;
 using Xamarin.Forms;
 using Plugin.CurrentActivity;
+using Intent = global::Android.Content.Intent;
+using Uri = global::Android.Net.Uri;
+using MvvmCross;
+using MvvmCross.Navigation;
+using Chameleon.Core.ViewModels;
+using Android.Provider;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chameleon.Android
 {
     [Activity(
-        Label = "Chameleon",
+        Label = "@string/app_name",
         Icon = "@mipmap/icon",
         Theme = "@style/MainTheme.Launcher",
         MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] { Intent.ActionSend, Intent.ActionSendMultiple }, Categories = new[] { Intent.CategoryDefault }, DataMimeTypes = new[] { "video/*", "audio/*" }, Label = "@string/app_name")]
     public class MainActivity : MvxFormsAppCompatActivity<Setup, Core.App, FormsApp>
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -35,7 +45,52 @@ namespace Chameleon.Android
 
             CrossMediaManager.Current.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            HandleIntent();
         }
+
+        private async void HandleIntent()
+        {
+            var receiverdIntent = Intent;
+            var receivedAction = receiverdIntent.Action;
+            var receivedType = receiverdIntent.Type;
+
+            if (receivedAction == Intent.ActionSend)
+            {
+                string path = "";
+
+                if (receivedType.StartsWith("video/") || receivedType.StartsWith("audio/"))
+                {
+                    var receiveUri = receiverdIntent.GetParcelableExtra(Intent.ExtraStream) as Uri;
+                    path = receiveUri.ToString();
+                }
+                if (!string.IsNullOrEmpty(path))
+                {
+                    await CrossMediaManager.Current.Play(path);
+                    await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<PlayerViewModel>();
+                }
+            }
+            else if(receivedAction == Intent.ActionSendMultiple)
+            {
+                IEnumerable<string> mediaUrls = null;
+
+                if (receivedType.StartsWith("video/") || receivedType.StartsWith("audio/"))
+                {
+                    var receiveUris = receiverdIntent.GetParcelableArrayListExtra(Intent.ExtraStream);
+                    mediaUrls = receiveUris.Cast<Uri>().Select(x => x.ToString());
+                }
+                if (mediaUrls != null)
+                {
+                    await CrossMediaManager.Current.Play(mediaUrls);
+                    await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<PlayerViewModel>();
+                }
+            }
+            else if (receivedAction.Equals(Intent.ActionMain))
+            {
+                Console.WriteLine("nothing shared");
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] global::Android.Content.PM.Permission[] grantResults)
         {
             Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);

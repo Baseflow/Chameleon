816c4cafab3b490676da4d5672030a3f29f98eec
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-08-12 12:56:54 +0200

    IList for OpenSourcePage

---
+++ Chameleon.Core/Helpers/OpenSourceButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MvvmCross.Commands;
+
+namespace Chameleon.Core.Helpers
+{
+    public class OpenSourceButton
+    {
+        public string Title { get; set; }
+        public MvxAsyncCommand OpenBrowserCommand { get; set; }
+        public string Url { get; set; }
+    }
+}
--- Chameleon.Core/ViewModels/OpenSourceViewModel.cs
+++ Chameleon.Core/ViewModels/OpenSourceViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Acr.UserDialogs;
+using Chameleon.Core.Helpers;
 using Chameleon.Services.Services;
 using MediaManager;
 using MediaManager.Media;
@@ -10,6 +11,7 @@ using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Chameleon.Core.ViewModels
 {
@@ -20,12 +22,102 @@ namespace Chameleon.Core.ViewModels
         public OpenSourceViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IUserDialogs userDialogs) : base(logProvider, navigationService)
         {
             _userDialogs = userDialogs ?? throw new ArgumentNullException(nameof(userDialogs));
+
         }
 
-        private IMvxAsyncCommand _mediaManagerCommand;
-        public IMvxAsyncCommand MediaManagerCommand => _mediaManagerCommand ?? (_mediaManagerCommand = new MvxAsyncCommand(() => Xamarin.Essentials.Browser.OpenAsync("https://github.com/martijn00/XamarinMediaManager")));
+        private List<OpenSourceButton> _openSourceButtons = new List<OpenSourceButton>();
+        public List<OpenSourceButton> OpenSourceButtons
+        {
+            get => _openSourceButtons;
+            set => SetProperty(ref _openSourceButtons, value);
+        }
 
-        private IMvxAsyncCommand _xamarinFormsCommand;
-        public IMvxAsyncCommand XamarinFormsCommand => _xamarinFormsCommand ?? (_xamarinFormsCommand = new MvxAsyncCommand(() => Xamarin.Essentials.Browser.OpenAsync("https://docs.microsoft.com/en-us/xamarin/xamarin-forms/")));
+
+        public override async Task Initialize()
+        {
+
+
+
+
+        }
+
+        public Task<IList<OpenSourceButton>> OpenSourceButtonsObject()
+        {
+            IList<OpenSourceButton> openSourceButtonsItem;
+            if (openSourceButtonsItem != null)
+            {
+                foreach (var item in openSourceButtonsItem)
+                {
+                    var urlMediaManager = "https://github.com/martijn00/XamarinMediaManager";
+                    var title = Title;
+                    var command = OpenBrowserCommand;
+                    var mediaManger = urlMediaManager + title + command;
+                }
+            }
+
+
+            return Task.FromResult<OpenSourceButton>(openSourceButtonsItem);
+        }
+
+
+        //public Task<IList<OpenSourceButton>> OpenSourceButtonsObject()
+        //{
+        //    IList<OpenSourceButton> openSourceButtonsItem;
+        //    foreach (var item in openSourceButtonsItem)
+        //    {
+
+        //    }
+        //    var mediaManager = "https://github.com/martijn00/XamarinMediaManager";
+        //    var title = OpenSourceButton.;
+
+        //    return Task.FromResult<OpenSourceButton>(urls);
+        //}
+
+        //public Task<IList<OpenSourceButton>> OpenSourceButtonsObject()
+        //{
+        //    var myUrl = CommonValues.URL + "images/Uploads/e0111.png";
+
+        //    var mediaManager = "https://github.com/martijn00/XamarinMediaManager";
+        //    var title = OpenSourceButton.;
+
+
+
+
+        //    "https://github.com/martijn00/XamarinMediaManager", "https://docs.microsoft.com/en-us/xamarin/xamarin-forms/"
+
+
+        //    IList<string> urls = new List<string>();
+
+        //    string[] url = { "https://github.com/martijn00/XamarinMediaManager", "https://docs.microsoft.com/en-us/xamarin/xamarin-forms/" };
+
+        //    foreach (string strings in url)
+        //    {
+        //        urls.Add(strings);
+        //    }
+
+            //for (int i = 0; i < urls.Count; i++) { 
+                
+            //    //  // Inflate the tile
+            //    var tile = LayoutInflater.Inflate(Resource.Layout.Tile, null);
+
+            //    //    // Set its attributes
+            //    //    tile.FindViewById<TextView>(Resource.Id.projectName).Text = currentProject;
+
+            //    //  // Add the tile
+            //    //  projectScrollView.AddView(tile);
+                //}
+
+                return Task.FromResult(urls);
+        }
     }
+
+        
 }
+
+
+       
+
+            
+
+
+

07cc1ffae8b9b4fb3cd89f865646f9aec43a727f
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-08-13 14:31:19 +0200

    added custom render for SeparatorColor to change color

---
+++ Chameleon.Core/ColoredTableView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Chameleon.Core
+{
+    public partial class ColoredTableView : TableView
+    {
+        public static BindableProperty SeparatorColorProperty = BindableProperty.Create("SeparatorColor", typeof(Color), typeof(ColoredTableView), Color.White);
+        public Color SeparatorColor
+        {
+            get
+            {
+                return (Color)GetValue(SeparatorColorProperty);
+            }
+            set
+            {
+                SetValue(SeparatorColorProperty, value);
+            }
+        }
+        public ColoredTableView()
+        {
+            //InitializeComponent();
+        }
+
+       
+    }
+}

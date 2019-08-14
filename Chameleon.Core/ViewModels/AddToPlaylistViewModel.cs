f949b6dd9cf8000bfcc3bdf080be4fe4a7784537
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-08-14 13:32:20 +0200

    Layout AddToPlaylistPage first commit

--- Chameleon.Core/ViewModels/AddToPlaylistViewModel.cs
+++ Chameleon.Core/ViewModels/AddToPlaylistViewModel.cs
@@ -6,6 +6,7 @@ using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
+using Xamarin.Forms;
 
 namespace Chameleon.Core.ViewModels
 {
@@ -25,11 +26,24 @@ namespace Chameleon.Core.ViewModels
         private IMvxAsyncCommand<IPlaylist> _addToPlaylistCommand;
         public IMvxAsyncCommand<IPlaylist> AddToPlaylistCommand => _addToPlaylistCommand ?? (_addToPlaylistCommand = new MvxAsyncCommand<IPlaylist>(AddToPlaylist));
 
+        private IMvxAsyncCommand<IPlaylist> _addNewPlaylistCommand;
+        public IMvxAsyncCommand<IPlaylist> AddNewPlaylistCommand => _addNewPlaylistCommand ?? (_addNewPlaylistCommand = new MvxAsyncCommand<IPlaylist>(AddNewPlaylist));
+
         private IMediaItem _selectedItem;
         public IMediaItem SelectedItem
         {
             get => _selectedItem;
             set => SetProperty(ref _selectedItem, value);
+        }
+
+        private string _addNewPlaylistName;
+        public string AddNewPlaylistName
+        {
+            get => _addNewPlaylistName;
+            set
+            {
+                SetProperty(ref _addNewPlaylistName, value);
+            }
         }
 
         public override async Task Initialize()
@@ -47,9 +61,22 @@ namespace Chameleon.Core.ViewModels
             await NavigationService.Close(this);
         }
 
+
+        private async Task AddNewPlaylist(IPlaylist arg)
+        {
+            arg.Add((MediaManager.Library.IMediaItem)Playlists);
+
+            //TODO: Save playlist
+            //await _playlistService.Save();
+
+            await NavigationService.Close(this);
+        }
+
         public override void Prepare(IMediaItem parameter)
         {
             _mediaItem = parameter;
         }
+
+     
     }
 }

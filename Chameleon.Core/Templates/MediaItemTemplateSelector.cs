45c2f4fa2a193f2b3f35dc00ca88aa98ca2db327
Rick van Dijk <rickavdijk@gmail.com>
2019-08-15 15:57:06 +0200

    CHAM-55 datatemplate selector added

---
+++ Chameleon.Core/Templates/MediaItemTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using MediaManager;
+using MediaManager.Library;
+using Xamarin.Forms;
+
+namespace Chameleon.Core.Templates
+{
+    public class MediaItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ActiveTemplate { get; set; }
+        public DataTemplate InActiveTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var currentItem = CrossMediaManager.Current.MediaQueue.Current.MediaUri;
+            var mediaItem = ((IMediaItem)item).MediaUri;
+            var b = currentItem == mediaItem ? ActiveTemplate : InActiveTemplate;
+            return b;
+        }
+    }
+}

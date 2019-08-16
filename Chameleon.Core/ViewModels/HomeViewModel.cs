e1963e50219f4ee83d53fccf51ac0b84d6961bc0
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-16 17:32:01 +0200

    Remove duplicate code

--- Chameleon.Core/ViewModels/HomeViewModel.cs
+++ Chameleon.Core/ViewModels/HomeViewModel.cs
@@ -64,9 +64,6 @@ namespace Chameleon.Core.ViewModels
         private IMvxAsyncCommand _addPlaylistCommand;
         public IMvxAsyncCommand AddPlaylistCommand => _addPlaylistCommand ?? (_addPlaylistCommand = new MvxAsyncCommand(AddPlaylist));
 
-        private IMvxAsyncCommand _addPlaylistCommand;
-        public IMvxAsyncCommand AddPlaylistCommand => _addPlaylistCommand ?? (_addPlaylistCommand = new MvxAsyncCommand(AddPlaylist));
-
         private IMvxAsyncCommand _openUrlCommand;
         public IMvxAsyncCommand OpenUrlCommand => _openUrlCommand ?? (_openUrlCommand = new MvxAsyncCommand(OpenUrl));
 
@@ -145,7 +142,7 @@ namespace Chameleon.Core.ViewModels
         private async Task AddPlaylist()
         {
             var config = new PromptConfig();
-            config.Message = "Enter the name of your new playlist";
+            config.Message = GetText("EnterNewName");
             var result = await _userDialogs.PromptAsync(config);
             if (result.Ok && !string.IsNullOrEmpty(result.Value))
             {

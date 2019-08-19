39ec4b8b768317cd76aa1c3bbd42164a86787c6f
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-19 23:41:00 +0200

    Add playback settings

---
+++ Chameleon.Core/Pages/SettingsPlaybackPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using Chameleon.Core.ViewModels;
+using MvvmCross.Forms.Presenters.Attributes;
+using MvvmCross.Forms.Views;
+using Xamarin.Forms;
+
+namespace Chameleon.Core.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    [MvxContentPagePresentation(WrapInNavigationPage = true)]
+    public partial class SettingsPlaybackPage : MvxContentPage<SettingsPlaybackViewModel>
+    {
+        public SettingsPlaybackPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

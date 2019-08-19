3367bc945ae99329172f225c6ecb7698066dea95
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-19 22:38:47 +0200

    Add provider sample

--- Chameleon.Core/Pages/ProvidersPage.xaml.cs
+++ Chameleon.Core/Pages/ProvidersPage.xaml.cs
@@ -8,7 +8,7 @@ namespace Chameleon.Core.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    [MvxContentPagePresentation(WrapInNavigationPage = true)]
+    [MvxModalPresentation(WrapInNavigationPage = true)]
     public partial class ProvidersPage : MvxContentPage<ProvidersViewModel>
     {
         public ProvidersPage()
--- Chameleon.Core/ViewModels/ProvidersViewModel.cs
+++ Chameleon.Core/ViewModels/ProvidersViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Chameleon.Services.Models;
 using MediaManager.Library;
 using MvvmCross.Commands;
 using MvvmCross.Logging;
@@ -14,27 +16,53 @@ namespace Chameleon.Core.ViewModels
         {
         }
 
-        private IMediaItem _selectedMediaItem;
-        public IMediaItem SelectedMediaItem
+        private Provider _selectedItem;
+        public Provider SelectedItem
         {
-            get => _selectedMediaItem;
-            set => SetProperty(ref _selectedMediaItem, value);
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
         }
 
-                private MvxObservableCollection<IMediaItem> _recommendedItems = new MvxObservableCollection<IMediaItem>();
-        public MvxObservableCollection<IMediaItem> RecommendedItems
+        private IList<Provider> _recommendedProviders;
+        public IList<Provider> RecommendedProviders
         {
-            get => _recommendedItems;
-            set => SetProperty(ref _recommendedItems, value);
+            get => _recommendedProviders;
+            set => SetProperty(ref _recommendedProviders, value);
         }
 
-        private IMvxAsyncCommand _sourceCommand;
-        public IMvxAsyncCommand SourceCommand => _sourceCommand ?? (_sourceCommand = new MvxAsyncCommand(PlayWhenSourceSelected));
-
-        private async Task PlayWhenSourceSelected()
+        private IList<Provider> _providers;
+        public IList<Provider> Providers
         {
-            await NavigationService.Navigate<PlayerViewModel, IMediaItem>(SelectedMediaItem);
-            SelectedMediaItem = null;
+            get => _providers;
+            set => SetProperty(ref _providers, value);
+        }
+
+        public override void Prepare()
+        {
+            base.Prepare();
+            Providers = new List<Provider>() {
+                new Provider(){ Title = "Internet Radio" },
+                new Provider(){ Title = "Podcasts" },
+                new Provider(){ Title = "Youtube" },
+                new Provider(){ Title = "Spotify" },
+                new Provider(){ Title = "Tidal" },
+                new Provider(){ Title = "Soundcloud" },
+                new Provider(){ Title = "URL Source" }
+            };
+
+            RecommendedProviders = new List<Provider>() {
+                new Provider(){ Title = "Internet Radio" },
+                new Provider(){ Title = "URL Source" }
+            };
+        }
+
+        private IMvxAsyncCommand<Provider> _sourceCommand;
+        public IMvxAsyncCommand<Provider> SourceCommand => _sourceCommand ?? (_sourceCommand = new MvxAsyncCommand<Provider>(OpenProvider));
+
+        private async Task OpenProvider(Provider provider)
+        {
+            //await NavigationService.Navigate<SourceViewModel, Provider>(SelectedItem);
+            SelectedItem = null;
         }
     }
 }

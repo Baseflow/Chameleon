98488cd99564b7faac93f980e7d237ceed54bd62
Rick van Dijk <rickavdijk@gmail.com>
2019-08-21 14:42:44 +0200

    CHAM-79 first version UI created for miniplayer

---
+++ Chameleon.Core/ViewModels/MiniPlayerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MediaManager;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+
+namespace Chameleon.Core.ViewModels
+{
+    public class MiniPlayerViewModel : BaseViewModel
+    {
+        public IMediaManager MediaManager { get; }
+
+        public MiniPlayerViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IMediaManager mediaManager)
+            : base(logProvider, navigationService)
+        {
+            MediaManager = mediaManager ?? throw new ArgumentNullException(nameof(mediaManager));
+        }   
+    }
+}
---
+++ Chameleon.Core/Views/MiniPlayerView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chameleon.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MiniPlayerView : ContentView
+    {
+        public MiniPlayerView()
+        {
+            InitializeComponent();
+        }
+    }
+}

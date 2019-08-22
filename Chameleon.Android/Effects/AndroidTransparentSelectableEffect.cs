487bd0afd35efbd59a2a242151cd6898dc638ae9
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-22 12:42:19 +0200

    Add sample for click ripple effect

---
+++ Chameleon.Android/Effects/AndroidTransparentSelectableEffect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Util;
+using Chameleon.Android.Effects;
+using Chameleon.Core.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("Chameleon")]
+[assembly: ExportEffect(typeof(AndroidTransparentSelectableEffect), nameof(TransparentSelectableEffect))]
+namespace Chameleon.Android.Effects
+{
+    public class AndroidTransparentSelectableEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var value = new TypedValue();
+                global::Android.App.Application.Context.Theme.ResolveAttribute(global::Android.Resource.Attribute.SelectableItemBackground, value, true);
+                Control.SetBackgroundResource(value.ResourceId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ Chameleon.Core/Effects/TransparentSelectableEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Chameleon.Core.Effects
+{
+    public class TransparentSelectableEffect : RoutingEffect
+    {
+        public TransparentSelectableEffect() : base("Chameleon.TransparentSelectableEffect")
+        {
+        }
+    }
+}

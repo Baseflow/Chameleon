e98fa029c4933f4551787b81bcfbbd07cb1c911f
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-22 18:01:15 +0200

    Open app as default as well

--- Chameleon.Android/MainActivity.cs
+++ Chameleon.Android/MainActivity.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
@@ -21,7 +24,7 @@ namespace Chameleon.Android
         Theme = "@style/MainTheme.Launcher",
         MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    [IntentFilter(new[] { Intent.ActionSend, Intent.ActionSendMultiple }, Categories = new[] { Intent.CategoryDefault }, DataMimeTypes = new[] { "video/*", "audio/*" }, Label = "@string/app_name")]
+    [IntentFilter(new[] { Intent.ActionSend, Intent.ActionSendMultiple, Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable, Intent.CategoryAppMusic }, DataMimeTypes = new[] { "video/*", "audio/*" }, Label = "@string/app_name")]
     public class MainActivity : MvxFormsAppCompatActivity<Setup, Core.App, FormsApp>
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -37,16 +40,77 @@ namespace Chameleon.Android
 
             CrossMediaManager.Current.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+        }
 
+        protected override void OnResume()
+        {
+            base.OnResume();
             HandleIntent();
         }
 
         private async void HandleIntent()
         {
-            if (await CrossMediaManager.Android.HandleIntent(Intent))
+            //TODO: Use next mediamanager update
+            //if (await CrossMediaManager.Android.PlayFromIntent(Intent))
+            if (await HandleIntent(Intent))
             {
                 await Mvx.IoCProvider.Resolve<IMvxNavigationService>().Navigate<PlayerViewModel>();
             }
+        }
+
+        public async Task<bool> HandleIntent(Intent intent)
+        {
+            if (intent == null)
+                return false;
+
+            var action = intent.Action;
+            var type = intent.Type;
+
+            if(action == Intent.ActionView)
+            {
+                string path = "";
+
+                if (type.StartsWith("video/") || type.StartsWith("audio/"))
+                {
+                    path = intent.DataString;
+                }
+                if (!string.IsNullOrEmpty(path))
+                {
+                    await CrossMediaManager.Current.Play(path);
+                    return true;
+                }
+            }
+            else if (action == Intent.ActionSend)
+            {
+                string path = "";
+
+                if (type.StartsWith("video/") || type.StartsWith("audio/"))
+                {
+                    var receivedUri = intent.GetParcelableExtra(Intent.ExtraStream) as global::Android.Net.Uri;
+                    path = receivedUri?.ToString();
+                }
+                if (!string.IsNullOrEmpty(path))
+                {
+                    await CrossMediaManager.Current.Play(path);
+                    return true;
+                }
+            }
+            else if (action == Intent.ActionSendMultiple)
+            {
+                IEnumerable<string> mediaUrls = null;
+
+                if (type.StartsWith("video/") || type.StartsWith("audio/"))
+                {
+                    var receivedUris = intent.GetParcelableArrayListExtra(Intent.ExtraStream);
+                    mediaUrls = receivedUris.Cast<global::Android.Net.Uri>().Select(x => x?.ToString());
+                }
+                if (mediaUrls != null)
+                {
+                    await CrossMediaManager.Current.Play(mediaUrls);
+                    return true;
+                }
+            }
+            return false;
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] global::Android.Content.PM.Permission[] grantResults)

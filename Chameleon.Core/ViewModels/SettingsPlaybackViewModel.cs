852216c9c6f0a30e28091e044c03a80214f33aed
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-08-23 07:17:20 +0200

    Progress settings playback page

--- Chameleon.Core/ViewModels/SettingsPlaybackViewModel.cs
+++ Chameleon.Core/ViewModels/SettingsPlaybackViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Acr.UserDialogs;
+using MediaManager;
+using MediaManager.Library;
+using MediaManager.Volume;
 using MvvmCross.Commands;
 using MvvmCross.Logging;
 using MvvmCross.Navigation;
@@ -9,10 +12,12 @@ namespace Chameleon.Core.ViewModels
     public class SettingsPlaybackViewModel : BaseViewModel
     {
         private readonly IUserDialogs _userDialogs;
+        public IMediaManager MediaManager { get; }
 
-        public SettingsPlaybackViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IUserDialogs userDialogs) : base(logProvider, navigationService)
+        public SettingsPlaybackViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IUserDialogs userDialogs, IMediaManager mediaManager) : base(logProvider, navigationService)
         {
             _userDialogs = userDialogs ?? throw new ArgumentNullException(nameof(userDialogs));
+            MediaManager = mediaManager ?? throw new ArgumentNullException(nameof(mediaManager));
         }
     }
 }

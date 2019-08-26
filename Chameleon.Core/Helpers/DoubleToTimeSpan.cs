9295fd2300f48b1fa4e36787713442eea22740b3
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-08-26 08:27:57 +0200

    added converter

---
+++ Chameleon.Core/Helpers/DoubleToTimeSpan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Converters;
+
+namespace Chameleon.Core.Helpers
+{
+    public class DoubleToTimeSpan : MvxValueConverter<double, TimeSpan>
+    {
+        protected override TimeSpan Convert(double value, Type targetType,object parameter,CultureInfo culture)
+        {
+            return TimeSpan.FromSeconds(value);
+
+        }
+    }
+
+}

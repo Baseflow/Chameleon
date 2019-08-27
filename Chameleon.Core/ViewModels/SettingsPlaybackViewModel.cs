be6d9e963004ba302fe0f5e1844d5c3a6cd20669
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-08-27 13:18:31 +0200

    Left and right for balance label works

--- Chameleon.Core/ViewModels/SettingsPlaybackViewModel.cs
+++ Chameleon.Core/ViewModels/SettingsPlaybackViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Acr.UserDialogs;
 using MediaManager;
 using MediaManager.Library;
@@ -19,5 +20,51 @@ namespace Chameleon.Core.ViewModels
             _userDialogs = userDialogs ?? throw new ArgumentNullException(nameof(userDialogs));
             MediaManager = mediaManager ?? throw new ArgumentNullException(nameof(mediaManager));
         }
+
+        private string _balanceLabel;
+        public string BalanceLabel
+        {
+            get => _balanceLabel;
+            set => SetProperty(ref _balanceLabel, value);
+        }
+
+        private string _balance;
+        public string Balance
+        {
+            get => _balance;
+            set => SetProperty(ref _balance, value);
+        }
+
+        private IMvxCommand _balanceChangedCommand;
+        public IMvxCommand BalanceChangedCommand => _balanceChangedCommand ?? (_balanceChangedCommand = new MvxCommand(UpdateBalanceLabel));
+
+        private void UpdateBalanceLabel()
+        {
+            var balance = Convert.ToInt32(MediaManager.VolumeManager.Balance * 10);
+            var balanceString = "";
+
+            if (balance == 0)
+                balanceString = "0";
+
+            if (balance < 0)
+            {
+                balance *= -1;
+                balanceString = $"+{balance.ToString()} Left";
+            }
+            else if(balance > 0)
+            {
+                balanceString = $"+{balance.ToString()} Right";
+            }
+
+            Balance = balanceString;
+        }
+
+        public override void ViewAppearing()
+        {
+            base.ViewAppearing();
+
+            UpdateBalanceLabel();
+        }
     }
+
 }

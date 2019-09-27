f2f8bef35c0378dedf59db3d85da68d4d0a2f447
Odette Oosterkamp <the.flower.of.life@gmail.com>
2019-09-27 16:19:12 +0200

    In process adding Radio stations

---
+++ Chameleon.Services/Resources/RadioStations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Chameleon.Services.Resources
+{
+    public partial class RadioStations
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("stations")]
+        public List<RadioStation> Samples { get; set; }
+    }
+
+    public partial class RadioStation
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("uri", NullValueHandling = NullValueHandling.Ignore)]
+        public string Uri { get; set; }
+    }
+
+    public partial class RadioStations
+    {
+        public static Stream GetEmbeddedResourceStream(Assembly assembly, string resourceFileName)
+        {
+            var resourceNames = assembly.GetManifestResourceNames();
+
+            var resourcePaths = resourceNames
+                .Where(x => x.EndsWith(resourceFileName, StringComparison.CurrentCultureIgnoreCase))
+                .ToArray();
+
+            if (!resourcePaths.Any())
+            {
+                throw new Exception(string.Format("Resource ending with {0} not found.", resourceFileName));
+            }
+
+            if (resourcePaths.Count() > 1)
+            {
+                throw new Exception(string.Format("Multiple resources ending with {0} found: {1}{2}", resourceFileName, Environment.NewLine, string.Join(Environment.NewLine, resourcePaths)));
+            }
+
+            return assembly.GetManifestResourceStream(resourcePaths.Single());
+        }
+
+        public static string GetEmbeddedResourceString(Assembly assembly, string resourceFileName)
+        {
+            var stream = GetEmbeddedResourceStream(assembly, resourceFileName);
+
+            using (var streamReader = new StreamReader(stream))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+
+        public static string GetEmbeddedResourceString(string resourceFileName)
+        {
+            return GetEmbeddedResourceString(Assembly.GetCallingAssembly(), resourceFileName);
+        }
+
+        public static List<RadioStation> FromJson(string json) => JsonConvert.DeserializeObject<List<RadioStation>>(json);
+    }
+}
+

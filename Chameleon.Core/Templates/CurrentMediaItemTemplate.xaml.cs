97b41a2155b23ca83fd5309c0e3f478e4b283830
Martijn van Dijk <mhvdijk@gmail.com>
2019-10-02 17:32:41 +0200

    Fix templates

---
+++ Chameleon.Core/Templates/CurrentMediaItemTemplate.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Chameleon.Core.Templates
+{
+    public partial class CurrentMediaItemTemplate : ContentView
+    {
+        public CurrentMediaItemTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}

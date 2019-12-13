96874fa37a6464bdb8f3d3b6f07aa584bf7bc698
Martijn van Dijk <mhvdijk@gmail.com>
2019-12-13 14:33:18 +0100

    Update packages

---
+++ Chameleon.Tizen/Chameleon.Tizen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Chameleon.Core;
+using Xamarin.Forms;
+
+namespace Chameleon.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            LoadApplication(new FormsApp());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
